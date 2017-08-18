e95b667464cdeda9d9cc0b54127a371fcd03fb2c
Aanand Kainth <akainth015@gmail.com>
2017-08-18 00:13:11 -0700

    Site update

--- public_files/fragments/home.fragment.js
+++ public_files/fragments/home.fragment.js
@@ -1,1 +1,10 @@
+const textSwitcher = $('#header-text-switch');
+
+textSwitcher.statements = ["banner", "ad", "project", "poster"]; textSwitcher.currentStatement = 0;
+setInterval(function() {
+    textSwitcher.fadeOut(function() {
+        textSwitcher.text(textSwitcher.statements[++textSwitcher.currentStatement % textSwitcher.statements.length]);
+        textSwitcher.fadeIn();
+    });
+}, 2000);
 const laptopFrame = $('#laptop_frame');
